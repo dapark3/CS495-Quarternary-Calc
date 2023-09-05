6c814979fbb8915e2793dfdaa83d3751a1455913
jeorozc0 <jeorozco@bsu.edu>
2023-09-05 13:52:26 -0400

    Add logic class with operation functions

---
+++ src/main/java/CalculatorLogic.java
@@ -0,0 +1,34 @@
+public class CalculatorLogic {
+    private int convertBaseFourToBaseTen(String base4){
+       String radixConversion = Integer.toString(Integer.parseInt(base4, 4), 10);
+       return Integer.parseInt(radixConversion);
+    }
+
+    private int convertBaseTenToBaseFour(String base10){
+        String radixConversion = Integer.toString(Integer.parseInt(base10, 10), 4);
+        return Integer.parseInt(radixConversion);
+    }
+
+    public int sumOfNumbers(String num1, String num2){
+        String result = Integer.toString(convertBaseFourToBaseTen(num1) +  convertBaseFourToBaseTen(num2));
+        return convertBaseTenToBaseFour(result);
+    }
+
+    public int timesOfNumbers(String num1, String num2){
+        String result = Integer.toString(convertBaseFourToBaseTen(num1) *  convertBaseFourToBaseTen(num2));
+        return convertBaseTenToBaseFour(result);
+    }
+
+    public int divisionOfNumbers(String num1, String num2){
+        String result = Integer.toString(convertBaseFourToBaseTen(num1) /  convertBaseFourToBaseTen(num2));
+        return convertBaseTenToBaseFour(result);
+    }
+    public int minusOfNumbers(String num1, String num2){
+        String result = Integer.toString(convertBaseFourToBaseTen(num1) -  convertBaseFourToBaseTen(num2));
+        return convertBaseTenToBaseFour(result);
+    }
+
+
+
+
+}
